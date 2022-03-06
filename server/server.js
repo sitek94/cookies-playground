5a0067e88e3cd130e0a39e457ffaa00cef8cdb04
Maciek <msitkowski94@gmail.com>
2022-03-06 08:23:48 +0100

    init

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const server = express()
+
+server.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+server.listen(3333, () => {
+  console.log('Server started on port 3333')
+})
